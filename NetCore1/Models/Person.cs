b930fee2f6415abc74cedd6d08da7b049f95555c
shufangwen <shufangwen@12xue.com.cn>
2020-12-22 16:43:20 +0800

    更新说明

---
+++ NetCore1/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore1.Models
+{
+    public class Person
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary> 
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
